245b4c61c211dc499b1456deb4fa789a6ed6d41f
Smac01 <chandramohan.cs19@bitsathy.ac.in>
2021-07-04 19:25:02 +0530

    little modification

--- js/minieditor.js
+++ js/minieditor.js
@@ -4,26 +4,29 @@ var element_style = `
 `
 var minieditor_styles = `
 <style type="text/css">
-	.linenumber {
+	{} .linenumber {
 		width: 5ch; 
 		resize: none; overflow: hidden;
 		border: none; outline: none;
 		text-align: right;
 		color: #8e8e8e;
 		padding-right: 10px;
+		border-right: 1px solid #cccccc;
+		border-right-style: dashed;
 	}
-	.editable_area {
+	{} .editable_area {
 		width: 100%;
-		min-height: 150px;
+		min-height: 400px;
 		resize: vertical;
 		overflow: auto; 
 		border: none; outline: none;
 		tab-size: 4; 
 		-moz-tab-size: 4;
+		-o-tab-size: 4;
 		white-space: pre;
 	}
-	.linenumber, .editable_area {
-		line-height: 1.5; font-size: 14px;
+	{} .linenumber, {} .editable_area {
+		line-height: 1.5; font-size: 16px;
 	}
 </style>
 `
@@ -38,44 +41,48 @@ var minieditor_styles = `
 
 var MiniEditor = function(selector){
 	this.element = document.querySelector(selector);
-	this.linenumber = undefined;
-	this.editable_area = undefined;
+	this.selector = selector;
 	this.classes = ["linenumber","editable_area"];
 	return this;
 }
+
 // function return number of lines
 MiniEditor.prototype.lines = function(){
 	return this.editable_area.value.split("\n").length;
 }
+
 // function to build multiline editor 
 // textarea, t1, t2 
 MiniEditor.prototype.build = function(){
 	this.element.style.cssText = element_style;
-	document.head.innerHTML += minieditor_styles;
+	document.head.innerHTML += minieditor_styles.replaceAll('{}', this.selector);
+
 	linenumber = document.createElement('textarea');
 	editable_area = document.createElement('textarea');
-	linenumber.className = "linenumber";
-	editable_area.className = "editable_area";
 	this.element.appendChild(linenumber);
 	this.element.appendChild(editable_area);
+	
+	linenumber.className = this.classes[0];
+	editable_area.className = this.classes[1];
+	
 	linenumber.readOnly = true;
 	linenumber.textContent = "1";
-	function scroll_t1(e){
-		editable_area.scrollTop = linenumber.scrollTop;
+	editable_area.autofocus = true;
+	
+	editable_area.onscroll = function(){
+		this.previousElementSibling.scrollTop = this.scrollTop;
 	}
-	function scroll_t2(e){
-		linenumber.scrollTop = editable_area.scrollTop;
-	}
-	linenumber.addEventListener("scroll", scroll_t1, false);
-	editable_area.addEventListener("scroll", scroll_t2, false);
+	
 	editable_area.oninput = function(){
 		var lines = this.value.split('\n');
-		linenumber.value = "";
+		this.previousElementSibling.value = "";
 		for (var i = 1; i < (lines.length+1); i++) {
-			linenumber.value += i+"\n";
+			this.previousElementSibling.value += i+"\n";
 		}
 	}
+
 	this.linenumber = linenumber;
 	this.editable_area = editable_area;
+	
 	return 0;
 }
